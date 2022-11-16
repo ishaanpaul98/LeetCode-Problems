a6a2ded5efa36843cd342456ea6c8763f6fd451a
paul64 <ishaanpaul@hotmail.com>
2022-11-16 10:18:29 -0800

    Time: 3315 ms (62.65%), Space: 61 MB (80.72%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def mergeTriplets(self, triplets, target):
+        """
+        :type triplets: List[List[int]]
+        :type target: List[int]
+        :rtype: bool
+        """
+        if len(triplets) == 1:
+            if triplets[0] == target:
+                return True
+            else:
+                return False
+        
+        pos = set()
+        
+        for i, [x,y,z] in enumerate(triplets):
+            if x > target[0] or y > target[1] or z > target[2]:
+                continue
+            else:
+                pos.add(i)
+            
+        print(pos)
+        a,b,c = 0,0,0
+        for i in pos:
+            a = max(a, triplets[i][0])
+            b = max(b, triplets[i][1])
+            c = max(c, triplets[i][2])
+        
+        #print(a)
+        #print(b)
+        #print(c)
+        print(a == target[0] or b == target[1])
+        return a == target[0] and b == target[1] and c == target[2]
+        
+        
+        
